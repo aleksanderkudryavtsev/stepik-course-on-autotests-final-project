fed38d8d281b7eda769101ea3f35b491baabb4e6
Alex Kudryavtsev <al.kudriavtcev@gmail.com>
2020-01-27 23:37:31 +0300

    product_page.py and test_product_page.py added, base_page.py and locators.py modified

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,16 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    PRODUCT_IN_ADD_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    ADD_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div")
+    PRODUCT_COST = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    COST_IN_BASKET_COST_MESSAGE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    BASKET_COST_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(2) > div")
+    
+    
 #     LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
 #     LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
 #     PASSWORD_RESET = (By.CSS_SELECTOR, "#login_form > p > a")
---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.should_be_add_to_basket_button()
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+        
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
+                
+    def should_be_correct_product_in_add_to_basket_message(self):
+        self.should_be_add_to_basket_message()
+        product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+        product_in_add_to_basket_message = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_ADD_TO_BASKET_MESSAGE).text
+        assert product_in_add_to_basket_message == product_title, "Product title in the add to basket message is incorrect"
+
+    def should_be_add_to_basket_message(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_MESSAGE), "Add to basket message is not presented"
+        
+    def should_be_correct_cost_in_basket_cost_message(self):
+        self.should_be_basket_cost_message()
+        product_cost = self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text
+        cost_in_basket_cost_message = self.browser.find_element(*ProductPageLocators.COST_IN_BASKET_COST_MESSAGE).text
+        assert cost_in_basket_cost_message == product_cost, "Cost in the basket cost message is incorrect"
+
+    def should_be_basket_cost_message(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_COST_MESSAGE), "Basket cost message is not presented"
